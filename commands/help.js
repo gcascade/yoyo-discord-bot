f9304925709791daaac072f8dda32a1928b6d6af
gcascade <geoffroy.cascade@reseau.eseo.fr>
2022-02-06 22:56:03 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,16 +4,13 @@ module.exports = {
 	name: 'help',
 	description: 'Describe every command the bot knows',
 	execute(message, args) {
+		const fields = [];
+		message.client.commands.forEach(command => fields.push({ name: command.name, value: command.description }));
 		const embed = new Discord.MessageEmbed()
 			.setColor('#5BFFDA')
 			.setTitle('Help')
 			.setDescription('Here\'s a description of the commands you can use')
-			.addFields(
-				{ name: 'clear', value: 'Clear the chat messages' },
-				{ name: 'github', value: 'Send a link to the bot\'s Github page' },
-				{ name: 'play', value: 'Join the user\'s voice channel and play a video from Youtube' },
-				{ name: 'leave', value: 'Leave current voice channel' },
-			)
+			.addFields(fields)
 			.setFooter({ text: 'Commands can be invoked with !' });
 		message.channel.send({ embeds: [embed] });
 	},
